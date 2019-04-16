4392b0df17a7a12102cc6c0278985d12a74bee79
Nanz Nair <nanz89@gmail.com>
2019-04-17 11:51:18 +1200

    Loads initial data

--- client/api.js
+++ client/api.js
@@ -2,10 +2,8 @@ import request from "superagent";
 
 const museumURL = "https://public.enigma.com/api/snapshots/c8073df4-0374-4c98-acdb-e08ac0880eb6?&row_limit=200&row_offset=0&include_serialids=true";
 
-export function getArtifact(callback) {
+export function getArtifacts(callback) {
     request.get(museumURL)
-    .end((err, res) => {
-        callback(err, req.body)
-    })
+    .end((err, res) => {callback(err, res.body)})
 }
 
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react';
+import {getArtifacts} from '../api'
+import { get } from 'http';
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+export default class App  extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        count: null,
+        fields: [],
+        rows: [],
+        loadingData: true
+      }
+    this.renderArtifacts = this.renderArtifacts.bind(this)
+    
+  }
+
+  componentDidMount () {
+    getArtifacts(this.renderArtifacts)
+  }
+
+  renderArtifacts (err, artifact) {
+    this.setState(
+      {
+        count: artifact.table_rows.count,
+        loadingData: false
+      }
+    )
+
+    console.log(artifact)
+    // this.setState({
+    //   // rows: artifact || []
+    // })
+  }
+
+
+  render() { 
+
+    return (
+
+      <div>
+        {this.state.loadingData && "loading...."}
+        Artifact count {this.state.count}
+      </div>
+      );
+  }
 }
-
-export default App
-
+ 
