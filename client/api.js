60afcc6d60f9fdbed4e91e3617eefab4a74ecb37
Nanz Nair <nanz89@gmail.com>
2019-04-17 11:26:08 +1200

    initial commit

---
+++ client/api.js
@@ -0,0 +1,11 @@
+import request from "superagent";
+
+const museumURL = "https://public.enigma.com/api/snapshots/c8073df4-0374-4c98-acdb-e08ac0880eb6?&row_limit=200&row_offset=0&include_serialids=true";
+
+export function getArtifact(callback) {
+    request.get(museumURL)
+    .end((err, res) => {
+        callback(err, req.body)
+    })
+}
+
